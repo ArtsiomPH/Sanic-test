62a3f122c80602640664787e743286f81a737ab0
Artsiom_PH <workartpharm@gmail.com>
2022-11-01 12:15:18 +0300

    add goods creating

--- goods.py
+++ goods.py
@@ -1,4 +1,4 @@
-from sanic import Blueprint, Request, json
+from sanic import Blueprint, Request, json, text
 
 from models import Goods
 from decorators import admin_only, active_user_only
@@ -17,4 +17,19 @@ async def get_goods_list(request: Request):
         result = await session.execute(select(Goods))
         goods_list = result.all()
 
-    return json([item[0].to_dict for item in goods_list])
+    return json([item[0].to_dict() for item in goods_list])
+
+
+@goods.post('/')
+@admin_only
+async def create_goods(request: Request):
+    body = request.json
+
+    goods_to_add = [Goods(**body)] if type(body) == dict else [Goods(**item) for item in body]
+
+    session = request.ctx.session
+    async with session.begin():
+        session.add_all(goods_to_add)
+
+    return json([item.to_dict() for item in goods_to_add])
+
